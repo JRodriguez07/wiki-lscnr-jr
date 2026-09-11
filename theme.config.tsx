137b357334d376905040aa88a149fad15b9b5bca
Lorenc <snellemany@gmail.com>
2026-09-11 02:08:42 +0200

    Update GitHub links in theme configuration

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,12 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>Lscnr</span>,
   project: {
-    link: 'https://github.com/IllInuz/wiki-lscnr',
+    link: 'https://github.com/Lorenc95/wiki-lscnr',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/IllInuz/wiki-lscnr',
+  docsRepositoryBase: 'https://github.com/Lorenc95/wiki-lscnr',
   footer: {
     text: '© 2024 LSCNR - All rights reserved.',
   },
